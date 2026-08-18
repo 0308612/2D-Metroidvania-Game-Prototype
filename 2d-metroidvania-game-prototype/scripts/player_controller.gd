3bf4b53a6703b11f2f9e8fbfa40288fdd09bac2b
Sebastian Schwenke <0308612@schoolsnet.act.edu.au>
2026-08-18 10:19:17 +1000

    fixed jumping, falling and landing animation and added walls and wall sliding and jumping

--- 2d-metroidvania-game-prototype/scripts/player_controller.gd
+++ 2d-metroidvania-game-prototype/scripts/player_controller.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @onready var player: CharacterBody2D = $"."
 @onready var animation_player: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_cast_left: RayCast2D = $"raycast/RayCast left"
+@onready var ray_cast_right: RayCast2D = $"raycast/RayCast right"
 
 @export_category("Movement Varibles")
 @export var Move_Speed:float = 600.0
@@ -16,13 +18,21 @@ var Movement = Vector2()
 @export var Jump_amount:int = 1
 @export var Jump_upgrade:bool = false
 
+var landing_animation_can_play:bool = false
+
+@export_category("Wall Logic Variables")
+@export var Wall_force_x:float = 1500.0
+@export var Wall_force_y:float = -1700.0
+var is_wall_jumping:bool = false
+
 func _physics_process(delta: float) -> void:
 	velocity.y += Gravity * delta
 	Horizontal_Movement()
 	Animations()
 	flip()
 	Jump_Logic()
-	print(velocity.y)
+	wall_logic()
+	
 	move_and_slide()
 
 func Horizontal_Movement():
@@ -34,18 +44,24 @@ func Horizontal_Movement():
 		velocity.x = move_toward(velocity.x, 0, Move_Speed * Deceleration)
 
 func Animations():
-	if velocity.x != 0 and velocity.y == 0:
+	if velocity.x != 0 and is_on_floor():
 		animation_player.play("Move")
-	if velocity.x == 0 and velocity.y == 0:
+	elif velocity.x == 0 and is_on_floor():
 		animation_player.play("Idle")
-	if is_on_floor() and velocity.y != 0:
+	if velocity.y != 0 and is_on_floor() and velocity.y == 0:
 		animation_player.play("Land")
+	if velocity.y < 0:
+		animation_player.play("Jump")
+	if velocity.y > 10 and is_on_floor() == false:
+		animation_player.play("fall")
 
 func flip():
 	if velocity.x > 0.0:
 		scale.x = scale.y * 1
+		Wall_force_x = 1500.0
 	elif velocity.x < 0.0:
 		scale.x = scale.y * -1
+		Wall_force_x = -1500.0
 
 func Jump_Logic():
 	if is_on_floor() and Jump_upgrade == false:
@@ -58,16 +74,24 @@ func Jump_Logic():
 	
 	if Input.is_action_just_pressed("jump") and Jump_amount > 0:
 		Jump_amount -= 1
-		animation_player.play("Jump")
 		velocity.y -= lerp(Jump_speed, Aceleration, 0.1)
 	
 	if not is_on_floor():
 		if Jump_amount > 0:
 			if Input.is_action_just_pressed("jump"):
-				animation_player.play("Jump")
-				velocity.y -= lerp(Jump_speed, Aceleration, 1)
+				velocity.y -= lerp(Jump_speed, Aceleration, 0.9)
 		if Input.is_action_just_released("jump"):
-			velocity.y = lerp(velocity.y, Gravity, 0.2)
-			velocity.y *= 0.3
+			velocity.y = lerp(velocity.y, Gravity, 0.3)
+			velocity.y *= 0.6
+			landing_animation_can_play = true
 	else:
 		return
+
+func wall_logic():
+	if is_on_wall_only():
+		velocity.y = 400
+		if Input.is_action_just_pressed("jump"):
+			if ray_cast_left.is_colliding():
+				velocity = Vector2(Wall_force_x, Wall_force_y)
+			if ray_cast_right.is_colliding():
+				velocity = Vector2(-Wall_force_x, Wall_force_y)
